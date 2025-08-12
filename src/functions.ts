e8d68d75249484f48ff8987ad1e87776a4696459
Azad Hossen <71219292+azad241@users.noreply.github.com>
2025-08-12 18:31:36 +0600

    Update functions.ts

--- src/functions.ts
+++ src/functions.ts
@@ -1,21 +1,5 @@
 export const getTicketTypeDisplay = (type: string) => {
-  switch (type) {
-    case 'free':
-      return 'Free'
-    case 'regular':
-      return 'Regular'
-    case 'premium':
-      return 'Premium'
-    case 'offer':
-      return 'Offer'
-    case 'vip':
-      return 'VIP'
-    case 'deluxe':
-      return 'Deluxe'
-    case 'custom':
-      return 'Custom'
-    default:
-      return type
-  }
-}
-
+  return type
+    .toLowerCase()
+    .replace(/\b\w/g, char => char.toUpperCase());
+};
